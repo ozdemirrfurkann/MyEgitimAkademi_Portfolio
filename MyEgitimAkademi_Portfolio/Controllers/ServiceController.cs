7590cf8456e2e4aa5dc4d0944962e53b9ea74645
Furkan Özdemir <ozdemirr.furkann@gmail.com>
2023-09-17 17:07:16 +0300

    links add

--- MyEgitimAkademi_Portfolio/Controllers/ServiceController.cs
+++ MyEgitimAkademi_Portfolio/Controllers/ServiceController.cs
@@ -22,5 +22,36 @@ namespace MyEgitimAkademi_Portfolio.Controllers
         {
             return View();
         }
+        [HttpPost]
+
+        public ActionResult AddService(Service service)
+        {
+            db.Service.Add(service);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteService(int id)
+        {
+            var value = db.Service.Find(id);
+            db.Service.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public ActionResult UpdateService(int id)
+        {
+            var value = db.Service.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateService(Service service)
+        {
+            var value = db.Service.Find(service.ServiceID);
+            value.ServiceName = service.ServiceName;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
